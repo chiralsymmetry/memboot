830c629783d38862189202d4f8f37e3f559933a4
David Nyman <david.nyman@protonmail.com>
2023-02-09 12:09:58 +0100

    Refactor index test

--- MemBoot.Tests/RingListTests.cs
+++ MemBoot.Tests/RingListTests.cs
@@ -182,8 +182,8 @@ namespace MemBoot.Tests
             new int[] { 2, 4, 1, 3, 0 })]
         [InlineData(10,
             new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
-            new int[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
-            new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+            new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+            new int[] { -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
         public void IndexOfShouldWork(int capacity, int[] itemsToAdd, int[] itemsToLookFor, int[] expectedIndices)
         {
             // Arrange
