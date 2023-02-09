8cff1ab621bcce1ad5724e4fdbcb949716ae79fb
David Nyman <david.nyman@protonmail.com>
2023-02-09 08:25:40 +0100

    Add ring list class with test

---
+++ MemBoot.Tests/RingListTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemBoot;
+using Xunit.Abstractions;
+
+namespace MemBoot.Tests
+{
+    public class RingListTests
+    {
+        [Fact]
+        public void EmptyListShouldHaveCountZero()
+        {
+            // Arrange
+            IList<string> ringList = new RingList<string>();
+            int expected = 0;
+
+            // Act
+            int actual = ringList.Count;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
