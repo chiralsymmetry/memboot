b90e762ab8b03598d89ddb89e84ad438832dc559
David Nyman <david.nyman@protonmail.com>
2023-02-11 12:09:02 +0100

    Add last n accuracy test

--- MemBoot.Tests/FlashcardTests.cs
+++ MemBoot.Tests/FlashcardTests.cs
@@ -97,5 +97,79 @@ namespace MemBoot.Tests
             Assert.Throws<ArgumentOutOfRangeException>(() => { flashcard.LastNAccuracy(-1); });
             Assert.Throws<ArgumentOutOfRangeException>(() => { flashcard.LastNAccuracy(11); });
         }
+
+        [Theory]
+        [InlineData(17,
+            new bool[] { },
+            0, 0)]
+        [InlineData(17,
+            new bool[] { false },
+            0, 0)]
+        [InlineData(17,
+            new bool[] { true },
+            0, 0)]
+        [InlineData(17,
+            new bool[] { true },
+            1, 1)]
+        [InlineData(17,
+            new bool[] { false, true, false },
+            0, 0)]
+        [InlineData(17,
+            new bool[] { false, true, false },
+            1, 0)]
+        [InlineData(17,
+            new bool[] { false, true, false },
+            2, 1.0 / 2.0)]
+        [InlineData(17,
+            new bool[] { false, true, false },
+            3, 1.0 / 3.0)]
+        [InlineData(17,
+            new bool[] { false, true, false, true, true, false, true, false, false },
+            0, 0)]
+        [InlineData(17,
+            new bool[] { false, true, false, true, true, false, true, false, false },
+            3, 1.0 / 3.0)]
+        [InlineData(17,
+            new bool[] { false, true, false, true, true, false, true, false, false },
+            8, 4.0 / 8.0)]
+        [InlineData(17,
+            new bool[] { false, true, false, true, true, false, true, false, false },
+            9, 4.0 / 9.0)]
+        [InlineData(17,
+            new bool[] { false, false, true, true, true, false, true, true, true, true, true, true, false, true, true, true, false, false, true, true, false, false, false, false, true },
+            0, 0)]
+        [InlineData(17,
+            new bool[] { false, false, true, true, true, false, true, true, true, true, true, true, false, true, true, true, false, false, true, true, false, false, false, false, true },
+            1, 1)]
+        [InlineData(17,
+            new bool[] { false, false, true, true, true, false, true, true, true, true, true, true, false, true, true, true, false, false, true, true, false, false, false, false, true },
+            3, 1.0 / 3.0)]
+        [InlineData(17,
+            new bool[] { false, false, true, true, true, false, true, true, true, true, true, true, false, true, true, true, false, false, true, true, false, false, false, false, true },
+            10, 4.0 / 10.0)]
+        [InlineData(17,
+            new bool[] { false, false, true, true, true, false, true, true, true, true, true, true, false, true, true, true, false, false, true, true, false, false, false, false, true },
+            16, 9.0 / 16.0)]
+        [InlineData(17,
+            new bool[] { false, false, true, true, true, false, true, true, true, true, true, true, false, true, true, true, false, false, true, true, false, false, false, false, true },
+            17, 10.0 / 17.0)]
+        public void LastNAccuracyShouldWork(int size, bool[] answersToGive, int n, float expectedAccuracy)
+        {
+            // Arrange
+            Flashcard flashcard = new(size);
+            float delta = 0.01f;
+            float expectedMin = Math.Max(expectedAccuracy - delta, 0);
+            float expectedMax = Math.Min(expectedAccuracy + delta, 1);
+
+            // Act
+            foreach (var answer in answersToGive)
+            {
+                flashcard.Answer(answer);
+            }
+            float actualAccuracy = flashcard.LastNAccuracy(n);
+
+            // Assert
+            Assert.InRange(actualAccuracy, expectedMin, expectedMax);
+        }
     }
 }
