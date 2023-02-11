8c8d3187c86149fa73ec7bf30e0ded2b76418b15
David Nyman <david.nyman@protonmail.com>
2023-02-11 11:27:23 +0100

    Add Flashcard class

---
+++ MemBoot/Flashcard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemBoot
+{
+    public class Flashcard
+    {
+        public string QuestionSide { get; set; } = string.Empty;
+        public string AnswerSide { get; set; } = string.Empty;
+        public bool IsComposable { get; set; } = false;
+        public RingList<bool> Answers { get; set; }
+        private readonly int AnswerHistorySize;
+        public float Accuracy => (float)Answers.Where(a => a).Count() / (float)AnswerHistorySize;
+
+        public Flashcard(int answerHistorySize = 100)
+        {
+            Answers = new(answerHistorySize);
+            AnswerHistorySize = answerHistorySize;
+        }
+
+        public void Answer(bool answer)
+        {
+            Answers.Add(answer);
+        }
+
+        public float LastNAccuracy(int n)
+        {
+            return (float)Answers.Skip(AnswerHistorySize - n).Where(a => a).Count() / (float)AnswerHistorySize;
+        }
+    }
+}
